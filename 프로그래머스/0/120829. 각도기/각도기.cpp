04aeba4b611dbd2ca0ff47fb6e5626b8eb650153
zzohyeyul <zzohyeyul@gmail.com>
2026-01-23 12:24:11 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+int solution(int angle) {
+    if (angle < 90) {
+        return 1;
+    }
+    else if (angle == 90) {
+        return 2;
+    }
+    else if (angle < 180) {
+        return 3;
+    }
+    else {
+        return 4;
+    }
+}
