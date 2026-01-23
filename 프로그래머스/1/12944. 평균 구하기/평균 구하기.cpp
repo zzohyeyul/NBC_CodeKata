bb9c7e165599791903842d45b304abd9de364fc2
zzohyeyul <zzohyeyul@gmail.com>
2026-01-23 12:25:39 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    int sum = 0;
+    
+    for (int i = 0; i < arr.size(); i++) {
+        sum += arr[i];
+    }
+    
+    return static_cast<double>(sum) / arr.size();
+}
