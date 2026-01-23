7697af7e5c20065a6b5caa51c3eb3dc13d710832
zzohyeyul <zzohyeyul@gmail.com>
2026-01-23 12:32:38 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    int n = x;
+    int sum = 0;
+
+    while (n > 0) {
+        sum += (n % 10);
+        n /= 10;
+    }
+
+    return (x % sum == 0);
+}
