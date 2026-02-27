1c8ebc162b0f5f870f3221d3ebc47b497e8b9104
zzohyeyul <zzohyeyul@gmail.com>
2026-02-27 12:15:31 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.39 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for (int s : score) {
+        pq.push(s);
+
+        if ((int)pq.size() > k) {
+            pq.pop();
+        }
+
+        answer.push_back(pq.top());
+    }
+
+    return answer;
+}
